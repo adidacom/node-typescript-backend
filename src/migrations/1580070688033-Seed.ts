3f921f37fd7ec2266870a3c800508ace0702c6e6
Dreamcoder <felipe1001costa@gmail.com>
2020-05-08 10:50:17 -0400

    Add migrations

---
+++ src/migrations/1580070688033-Seed.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, getRepository } from "typeorm";
+
+import { User } from "../models/User";
+
+export class Seed1580070688033 implements MigrationInterface {
+    public async up(): Promise<void> {
+        const user = getRepository(User).create({
+            username: 'user1',
+        });
+
+        await getRepository(User).save(user);
+    }
+
+    public async down(): Promise<void> {
+        const user = await getRepository(User).findOne({
+            where: { username: 'user1' },
+        });
+
+        await getRepository(User).remove(user);
+    }
+}
