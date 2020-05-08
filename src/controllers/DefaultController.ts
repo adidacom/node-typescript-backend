91fc8989a20238fc88ab1ac4bae71e1ef3723a96
Dreamcoder <felipe1001costa@gmail.com>
2020-05-08 10:52:20 -0400

    Update controllers

--- src/controllers/DefaultController.ts
+++ src/controllers/DefaultController.ts
@@ -1,10 +1,13 @@
-import { Response, Request } from "express";
-import {JsonController, Req, Res, Get } from "routing-controllers";
+import { Response, Request } from 'express';
+import { JsonController, Req, Res, Get } from 'routing-controllers';
 
 @JsonController()
 export class DefaultController {
-    @Get("/")
-    public helathCheck(@Req() req: Request, @Res() res: Response): Response<unknown> {
-        return res.send({ message: 'Hello World' });
-    }
+  @Get('/')
+  public helathCheck(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Response<unknown> {
+    return res.send({ message: 'Hello World' });
+  }
 }
--- src/controllers/ToDoListController.ts
+++ src/controllers/ToDoListController.ts
@@ -1,98 +1,119 @@
-import "reflect-metadata";
-import Boom from "@hapi/boom";
-import Joi from "@hapi/joi";
-import { Response, Request } from "express";
-import {JsonController, Req, Res, Get, Post, Delete, UseBefore, Put } from "routing-controllers";
+import 'reflect-metadata';
+import Boom from '@hapi/boom';
+import Joi from '@hapi/joi';
+import { Response, Request } from 'express';
+import {
+  JsonController,
+  Req,
+  Res,
+  Get,
+  Post,
+  Delete,
+  UseBefore,
+  Put,
+} from 'routing-controllers';
 
-import { Authenticate } from "../middleware/authenticate";
-import { ToDoItemService, ToDoListService } from "../services";
+import { Authenticate } from '../middleware/authenticate';
+import { ToDoItemService, ToDoListService } from '../services';
 
 @JsonController()
 @UseBefore(Authenticate)
 export class ToDoListController {
-    constructor(
-        private toDoItemService: ToDoItemService,
-        private toDoListService: ToDoListService) {}
+  constructor(
+    private toDoItemService: ToDoItemService,
+    private toDoListService: ToDoListService,
+  ) {}
 
-    @Get('/todos')
-    public async getToDoListsForUser(@Req() req: Request, @Res() res: Response): Promise<Response<unknown>> {
-        const { limit, offset } = await Joi
-            .object({
-                offset: Joi.number().integer().default(0).failover(0),
-                limit: Joi.number().integer().default(10).failover(10),
-            })
-            .validateAsync({
-                offset: req.query.offset,
-                limit: req.query.limit,
-            });
+  @Get('/todos')
+  public async getToDoListsForUser(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response<unknown>> {
+    const { limit, offset } = await Joi.object({
+      offset: Joi.number().integer().default(0).failover(0),
+      limit: Joi.number().integer().default(10).failover(10),
+    }).validateAsync({
+      offset: req.query.offset,
+      limit: req.query.limit,
+    });
 
-        const [lists, total] = await this.toDoListService.getToDoListsForUser(req.user.id, offset, limit);
+    const [lists, total] = await this.toDoListService.getToDoListsForUser(
+      req.user.id,
+      offset,
+      limit,
+    );
 
-        return res.send({
-            total,
-            data: lists.map((list) => ({ id: list.id, title: list.title })),
-        });
+    return res.send({
+      total,
+      data: lists.map((list) => ({ id: list.id, title: list.title })),
+    });
+  }
+
+  @Post('/todos')
+  public async createToDoListForUser(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response<unknown>> {
+    const { title, items } = await Joi.object({
+      title: Joi.string().min(3).max(255).required(),
+      items: Joi.array().items(Joi.string().min(3).max(255)),
+    }).validateAsync({
+      title: req.body.title,
+      items: req.body.items,
+    });
+
+    const list = await this.toDoListService.createToDoListForUser(
+      req.user.id,
+      title,
+    );
+
+    await this.toDoItemService.addTodoItems(list.id, items);
+
+    return res.status(201).send({ id: list.id, title: list.title });
+  }
+
+  @Put('/todos/:todolistId')
+  public async updateToDoList(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response<unknown>> {
+    const { todolistId, title } = await Joi.object({
+      todolistId: Joi.string().uuid().required(),
+      title: Joi.string().min(3).max(255).required(),
+    }).validateAsync({
+      todolistId: req.params.todolistId,
+      title: req.body.title,
+    });
+
+    let list = await this.toDoListService.getToDoList(todolistId);
+    if (list.userId !== req.user.id) {
+      throw Boom.forbidden('You do not have access to this resource');
     }
 
-    @Post('/todos')
-    public async createToDoListForUser(@Req() req: Request, @Res() res: Response): Promise<Response<unknown>> {
-        const { title, items } = await Joi
-            .object({
-                title: Joi.string().min(3).max(255).required(),
-                items: Joi.array().items(Joi.string().min(3).max(255)),
-            })
-            .validateAsync({
-                title: req.body.title,
-                items: req.body.items,
-            });
+    list.title = title;
+    list = await this.toDoListService.updateToDoList(list);
 
-        const list = await this.toDoListService.createToDoListForUser(req.user.id, title);
+    return res.send({ id: list.id, title: list.title });
+  }
 
-        await this.toDoItemService.addTodoItems(list.id, items);
+  @Delete('/todos/:todolistId')
+  public async removeToDoList(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response<unknown>> {
+    const { todolistId } = await Joi.object({
+      todolistId: Joi.string().uuid().required(),
+    }).validateAsync({
+      todolistId: req.params.todolistId,
+    });
 
-        return res.status(201).send({ id: list.id, title: list.title });
+    const list = await this.toDoListService.getToDoList(todolistId);
+    if (list.userId !== req.user.id) {
+      throw Boom.forbidden('You do not have access to this resource');
     }
 
-    @Put('/todos/:todolistId')
-    public async updateToDoList(@Req() req: Request, @Res() res: Response): Promise<Response<unknown>> {
-        const { todolistId, title } = await Joi
-            .object({
-                todolistId: Joi.string().uuid().required(),
-                title: Joi.string().min(3).max(255).required(),
-            })
-            .validateAsync({
-                todolistId: req.params.todolistId,
-                title: req.body.title,
-            });
+    await this.toDoListService.removeToDoList(list);
 
-        let list = await this.toDoListService.getToDoList(todolistId);
-        if (list.userId !== req.user.id) {
-            throw Boom.forbidden('You do not have access to this resource');
-        }
-
-        list.title = title;
-        list = await this.toDoListService.updateToDoList(list);
-
-        return res.send({ id: list.id, title: list.title });
-    }
-
-    @Delete('/todos/:todolistId')
-    public async removeToDoList(@Req() req: Request, @Res() res: Response): Promise<Response<unknown>> {
-        const { todolistId } = await Joi
-            .object({
-                todolistId: Joi.string().uuid().required(),
-            })
-            .validateAsync({
-                todolistId: req.params.todolistId,
-            });
-
-        const list = await this.toDoListService.getToDoList(todolistId);
-        if (list.userId !== req.user.id) {
-            throw Boom.forbidden('You do not have access to this resource');
-        }
-
-        await this.toDoListService.removeToDoList(list);
-
-        return res.send(204);
-    }
+    return res.send(204);
+  }
 }
