8148e16247d2cb64eb56f103cd7bc86a891de408
Dreamcoder <felipe1001costa@gmail.com>
2020-05-08 10:52:09 -0400

    Update middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,29 +1,34 @@
-import { Boom } from "@hapi/boom";
-import { ValidationError } from "@hapi/joi";
-import {Request, Response, NextFunction} from "express";
+import { Boom } from '@hapi/boom';
+import { ValidationError } from '@hapi/joi';
+import { Request, Response, NextFunction } from 'express';
 
-import config from "../config";
+import config from '../config';
 
-export default function ErrorHandler(err: Error, req: Request, res: Response, next: NextFunction): void {
-    const joiErr = err as ValidationError;
-    if (joiErr.isJoi) {
-        res.status(400).send({ message: joiErr.details[0].message });
-        return next();
-    }
+export default function ErrorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const joiErr = err as ValidationError;
+  if (joiErr.isJoi) {
+    res.status(400).send({ message: joiErr.details[0].message });
+    return next();
+  }
 
-    const boomErr = err as Boom;
-    if (boomErr.isBoom) {
-        res.status(boomErr.output.statusCode).send({ message: boomErr.message });
-        return next();
-    }
+  const boomErr = err as Boom;
+  if (boomErr.isBoom) {
+    res.status(boomErr.output.statusCode).send({ message: boomErr.message });
+    return next();
+  }
 
-    switch (err.name) {
+  switch (err.name) {
     default:
-        req.log.error(err, 'Something not handled well');
-        res.status(500).send({
-            message: err.message,
-            stack: config.env === 'development' ? err.stack : undefined,
-        });
-        return next();
-    }
+      req.log.error(err, 'Something not handled well');
+      res.status(500).send({
+        message: err.message,
+        stack: config.env === 'development' ? err.stack : undefined,
+      });
+      return next();
+  }
 }
